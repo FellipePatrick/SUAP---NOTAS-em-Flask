be220446aa34db2a0cd86440eb8a5b22e7b8d4df
pokytester <fellipe.patrick678@gmail.com>
2022-11-29 15:26:31 -0300

    up

--- projetoflask/app.py
+++ projetoflask/app.py
@@ -1,10 +1,60 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template
+from flask_oauthlib.client import OAuth
+
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+suap = oauth.remote_app(
+    'suap',
+    consumer_key="7F65kRtgwW2B6JJbXgSHnkAf75f9bHsmVbIOt1AO",
+    consumer_secret="iDUTqLzNaW8E7uALJ5RuaHfv6zSoyxHCPrwhrY9jlFfPT7iTyNwqjgt6KZHLz7SiYX5IKRC06PtOwL92TSmHd4pQb34mgiEymQp1q0bblmRHhk423tKxojaWPZAeZw9C",
+    base_url='https://suap.ifrn.edu.br/api/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://suap.ifrn.edu.br/o/token/',
+    authorize_url='https://suap.ifrn.edu.br/o/authorize/'
+)
+
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    if 'suap_token' in session:
+        me = suap.get('v2/minhas-informacoes/meus-dados')
+        return render_template('user.html', user_data=me.data)
+    else:
+        return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/login')
+def login():
+    return suap.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('suap_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = suap.authorized_response()
+    if resp is None or resp.get('access_token') is None:
+       return 'Access denied: reason=%s error=%s resp=%s' % (
+            request.args['error'],
+            request.args['error_description'],
+            resp
+        )
+    session['suap_token'] = (resp['access_token'], '')
+    return redirect(url_for('index')) 
+
+
+@suap.tokengetter
+def get_suap_oauth_token():
+    return session.get('suap_token')
+
+if __name__ == '__main__':
+    app.run()
